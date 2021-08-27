ed05551e1046fcf45a68250f0e6e89d67c1c17ff
2wndrhs <2wndrhs@naver.com>
2021-08-27 16:07:51 +0000

    Vanilla JS

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const title = document.getElementById('title');
+const title = document.querySelector('#title');
+const CLICKED_CLASS = "clicked";
+
+function handleClick(){
+    title.classList.toggle(CLICKED_CLASS);
+    // const hasClass = title.classList.contains(CLICKED_CLASS);
+    
+    // console.log(hasClass);
+    
+    // if(hasClass){
+    //     title.classList.remove(CLICKED_CLASS);
+    // }
+    
+    // else{
+    //     title.classList.add(CLICKED_CLASS);        
+    // }
+}
+
+function init(){
+    title.addEventListener("click", handleClick);
+}
+
+init();
+
+
